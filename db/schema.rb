9e925632e11726d84c761b960bdc1ef98db8931b
EC2 Default User <ec2-user@ip-172-31-33-216.ap-northeast-1.compute.internal>
2020-08-12 13:42:56 +0000

    error css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200812130239) do
+ActiveRecord::Schema.define(version: 20200812132957) do
+
+  create_table "books", force: :cascade do |t|
+    t.string   "title"
+    t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

6082588ed990543a7e8c0f631c10f6e713f00f10
EC2 Default User <ec2-user@ip-172-31-33-216.ap-northeast-1.compute.internal>
2020-08-13 14:36:52 +0000

    Add review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :reviews, only: [:index, :destroy]
   root 'books#index'
-  resources :books
+  resources :books do
+  resources :reviews, except: [:index, :destroy]
+  end
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200812132957) do
+ActiveRecord::Schema.define(version: 20200813141237) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "user_id",     null: false
+    t.integer  "book_id",     null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
